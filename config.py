439a0554e5a3c16357792a84cb69a0d9b7423194
yeha98555 <yeha98555@gmail.com>
2024-06-30 19:46:25 +0000

    feat: Add logging settings in config file

---
+++ config.py
@@ -0,0 +1,54 @@
+import logging
+import logging.config as log_config
+import sys
+from pathlib import Path
+
+# Directories
+ROOT_DIR = Path(__file__).parent.absolute()
+LOGS_DIR = Path(ROOT_DIR, "logs")
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+# Logger
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"  # noqa
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info", "error"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
+
+# Logger
+log_config.dictConfig(logging_config)
+logger = logging.getLogger()
